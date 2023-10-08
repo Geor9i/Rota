69facd9c5d9a70448dd236d4e2927a1e1a46561f
Georgi <gurumov93@gmail.com>
2023-10-08 23:58:53 +0100

    Rota can calculate wages, and hours per employee

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ class Rota {
         this.tmSchedule = {};
         this.schedule = {};
         this.extractTableData(tableArr);
+        this.hourlyPay = {
+            sr: 11.15,
+            tm: 10.65,
+        };
+        this.tax = 0.2;
     }
 
     extractTableData(tableArr) {
@@ -19,11 +24,11 @@ class Rota {
 
     tableToScheduleObj(trArr) {
         const shiftPattern = /\d{2}:\d{2}[\s-]{3}\d{2}:\d{2}/;
-        const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
             let schedule = {};
             trArr.forEach(tr => {
                 let tdArr = Array.from(tr.querySelectorAll('td')).slice(0,8).map(td => td.textContent);
-                let name = tdArr[0].toLowerCase();
+                let name = this.formatStrings(tdArr[0])
+                let weekdays = this.getWeekday([]);
                 schedule[name] = weekdays.reduce((shifts, day, i) => {
                     let data = tdArr[i + 1];
                     let match;
@@ -38,41 +43,143 @@ class Rota {
             return schedule;
         }
 
-        shiftLength(person, weekday) {
-            [person, weekday] = [person, weekday].map(str => str.toLowerCase());
-            if (!this.schedule[person] || !this.schedule[person][weekday]) return null;
+        formatStrings(...strings) {
+            if (strings.length === 1) {
+                return strings[0].trim().toLowerCase();
+            }
+            return strings.map(str => str.trim().toLowerCase())
+        }
 
-            let shiftArr = this.schedule[person][weekday];
-            let startTime = shiftArr[0];
-            let endTime = shiftArr[1];
+        getWeekday(data) {
+            let variations = {
+                monday: ['mon', 'monday', 'm', 'mo', '1'],
+                tuesday: ['tue', 'tuesday', 'tu', 't', '2'],
+                wednesday: ['wed', 'wednesday', 'we', 'w', '3'],
+                thursday: ['thu', 'thursday', 'thur', 'th', '4'],
+                friday: ['fri', 'friday', 'fr', 'f', '5'],
+                saturday: ['sat', 'saturday', 'sa', 's', '6'],
+                sunday: ['sun', 'sunday', 'su', '7'],
+            }
+            if (Array.isArray(data)) return Object.keys(variations);
+
+            let string = data.toLowerCase();
+
+            for (let day in variations) {
+                if(variations[day].includes(string)) {
+                    return day;
+                }
+            }
+        }
+
+        employee () {
+            return {
+                findByName: (name) => {
+                    name = this.formatStrings(name)
+                    let namesArr = Object.keys(this.schedule).filter(n => n.includes(name));
+                    if (namesArr.length === 1) {
+                        let fullName = namesArr[0];
+                        return this.schedule[fullName]
+                    } else if (namesArr.length > 1) {
+                        throw new Error ('More than one person in rota! Please enter fullName!')
+                    } else {
+                        throw new Error (`Cannot find ${name}`);
+                    }
+                },
+                get: (person, options) => {
+                    if (typeof person === 'string') {
+                        person = this.employee().findByName(person);
+                    }
+                    if (options && options.position) {
+                        return [person, this.employee().getPosition(person)]
+                    }
+                    return person;
+                },
+                getPosition: (name) => {
+                    let obj = this.employee().get(name);
+                    let result;
+                    if ((result = this.employee().findByObj(obj, this.managerSchedule))!== null) {
+                        return 'sr';
+                    } else if ((result = this.employee().findByObj(obj, this.tmSchedule))!== null) {
+                        return 'tm';
+                    }
+                    return null;
+                },
+                findByObj: (personObj, rota) => {
+                    let find = (obj, schedule = this.schedule) => {
+                        let scheduleArr = Object.values(schedule);
+                        let result = scheduleArr.find((el) => {
+                            return JSON.stringify(obj) === JSON.stringify(el)
+                        });
+                        return result ? result : null;
+                    }
+                    
+                    if (!rota) {
+                        return find(personObj);
+                    } else {
+                        return find(personObj, rota)
+                    }
+                }
+            }
+        }
+
+        shiftLength(person, weekday, options) {
+            person = this.employee().get(person);
+            weekday = this.getWeekday(weekday);
+            if (!this.employee().findByObj(person) || !person[weekday]) return null;
+
+            let startTime = person[weekday][0];
+            let endTime = person[weekday][1];
             //Add 24h to end time if the shift spans more than one day
-            let result = this.time().select(endTime).isBiggerThan(startTime)
-            console.log(result);
+            let result = this.time().difference(startTime, endTime);
+            if (options && options.minutes) {
+                return result;
+            }
+            return result = this.time().toHours(result); 
         }
 
         time () {
             return {
                 difference(startTime, endTime) {
+                    if (endTime.split(':')[0] === '00') {
+                        endTime = `${24}:${endTime.split(':')[1]}`;
+                    }
                     return this.toMinutes(endTime) - this.toMinutes(startTime);
                 },
                 toMinutes(time) {
                     let obj = this.toObj(time);
                     return obj.h * 60 + obj.m;
                 },
-                select(time) {
-                    let that = this;
+                toHours(minutes) {
+                    let hours = Math.trunc(Math.max(0, minutes / 60));
                     return {
-                        isEq(compareTime) {
-                            let compare = that.toMinutes(compareTime);
-                            return that.toMinutes(time) === compare;
+                        h: hours,
+                        m: minutes - (hours * 60)
+                    }
+                },
+                getBreak(shift) {
+                    let minutes = this.difference(shift[0], shift[1]);
+                    if (minutes > 360 ) {
+                        return 30;
+                    }else if (minutes > 240 ) {
+                        return 15;
+                    } else {
+                        return 0
+                    }
+                },
+                select(time) {
+                    // let that = this;
+                    return {
+                        isEq: (compareTime) => {
+                            let compare = this.toMinutes(compareTime);
+                            return this.toMinutes(time) === compare;
                         },
-                        isBiggerThan(compareTime) {
-                            let compare = that.toMinutes(compareTime);
-                            return that.toMinutes(time) > compare;
+                        isBiggerThan: (compareTime) => {
+                            let compare = this.toMinutes(compareTime);
+                            return this.toMinutes(time) > compare;
                         },
-                        isLessThan(compareTime) {
-                            let compare = that.toMinutes(compareTime);
-                            return that.toMinutes(time) < compare;
+                        isLessThan: (compareTime) => {
+                            let compare = this.toMinutes(compareTime);
+                            return this.toMinutes(time) < compare;
                         }
                     };
                 },
@@ -86,45 +193,127 @@ class Rota {
             }
         }
 
-        weekDayShifts(weekday) {
-            weekday = weekday.toLowerCase();
+        weekDayRota(weekday, options) {
+            weekday = this.getWeekday(weekday);
             let result = {};
+            let breakMinutes = 0;
             for (let person in this.schedule) {
                 if (this.schedule[person][weekday]) {
-                    result[person] = this.schedule[person][weekday];
+                    let shift = this.schedule[person][weekday];
+                    breakMinutes += this.time().getBreak(shift);
+                    result[person] = shift;
                 }
+            }
+            if (options) {
+                let totalMinutes = [];
+                for (let person in result) {
+                    let [start, end] = result[person];
+                    totalMinutes.push(this.time().difference(start, end))
+                }
+                totalMinutes = totalMinutes.reduce((a, c) => a += c);
+                if (options.hours) {
+                    return this.time().toHours(totalMinutes)
+                } else if (options.minutes) {
+                    return totalMinutes;
+                } else if (options.breaks) {
+                    return breakMinutes;
+                }
+                return null;
             }
             return result
         }
 
-        getPersonHours(person) {
-            if (!this.schedule[person]) return null;
-                
-            let workHours = [];
-            for(let day in this.schedule[person]) {
-                if (this.schedule[person][day] !== '') {
-                    workHours.push(this.shiftLength(person, day));
+        personHours(person, options) {
+            let employee = this.employee().get(person);
+            let totalMinutes = [];
+            for(let day in employee) {
+                if (employee[day] !== '') {
+                    if (options) {
+                        let breaks = this.time().getBreak(employee[day]);
+                        let dailyHours = this.time().difference(...employee[day])
+                        if (options.breaks) {
+                            totalMinutes.push(breaks);
+                        } else if (options.paid) {
+                            totalMinutes.push(dailyHours - breaks);
+                        } 
+                    } else {
+                        totalMinutes.push(dailyHours);
+                    }
                 }
             }
-            return workHours;
-        }
-
-        wage(person) {
-            person = person.toLowerCase();
-            let hourlyPay = {
-                sr: 11.15,
-                tm: 10.65,
+            totalMinutes = totalMinutes.reduce((acc, curr) => acc += curr, 0);
+            if (options && options.minutes) {
+                return totalMinutes;
             }
-            let pay = this.managerSchedule.hasOwnProperty(person)
-            ? hourlyPay.sr
-            : hourlyPay.tm;
-
+            return this.time().toHours(totalMinutes);
         }
+
+        totalHours(options) {
+            let weekdays = this.getWeekday([]);
+            let totalMinutes = [];
+            let breakMinutes = 0;
+            for (let day of weekdays) {
+                breakMinutes += this.weekDayRota(day, {breaks:true});
+                totalMinutes.push(this.weekDayRota(day, {minutes: true}));
+            }
+            totalMinutes = totalMinutes.reduce((acc, curr) => acc += curr);
+
+            if (options) {
+              if (options.paid) {
+                    return options.minutes ? totalMinutes - breakMinutes
+                    : this.time().toHours(totalMinutes - breakMinutes);
+                } else if (options.breaks) {
+                    if (options.minutes) {
+                        return breakMinutes;
+                    }
+                    return this.time().toHours(breakMinutes);
+                } else if (options.minutes) {
+                    return totalMinutes;
+                } 
+            }
+            return this.time().toHours(totalMinutes);
+        }
+
+        labour(options) {
+            let specialPay = {
+                georgi: 11.65,
+                callum: 12.60
+            }
+            let labour = 0;
+            for (let person in this.schedule) {
+                let position = this.employee().getPosition(person); 
+                let pay = specialPay[person] ? specialPay[person]
+                : this.hourlyPay[position];
+                labour += this.wage(person, options);
+            }
+            return labour;
+        }
+
+        wage(person, options) {
+            let [employee, position] = this.employee().get(person, {position: true});
+            let pay = options?.pay ?options.pay : this.hourlyPay[position];
+            let totalHours = this.personHours(person, {paid:true});
+            let weeklyPay = totalHours.h * pay + (totalHours.m / 60) * pay;
+            if (options) {
+                if (options.net) {
+                return weeklyPay - weeklyPay * this.tax
+                }else if (options.month){
+                    let monthly = (weeklyPay / 7) * 30;
+                    return options.net ? monthly - (monthly * this.tax) : monthly;
     
+                } else if (options.year) {
+                    let yearly = ((weeklyPay / 7) * 30) * 12;
+                    return options.net ? yearly - (yearly * this.tax)
+                    : yearly;
+            }
+            } else {
+                return weeklyPay
+            }
+        }
 }
 
 
 const rota = new Rota(tableArr);
 
-// console.log(rota.getPersonHours('georgi'));
-console.log(rota.shiftLength('kelly', 'monday'));
+// console.log(rota.wage('sean', {year:true, net:true}));
+console.log(rota.labour());
